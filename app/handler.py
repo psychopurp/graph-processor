d25af8f0c016ff93c41e265160422886f87e21a6
Elyar <1595685558@qq.com>
2021-03-12 21:58:05 +0800

    迁移路由完成

--- app/handler.py
+++ app/handler.py
@@ -1,4 +1,5 @@
 from flask import jsonify, Blueprint
+import task
 
 
 handler_print = Blueprint("handler", __name__)
@@ -8,6 +9,26 @@ def response(st, msg, data):
     return jsonify({"st": st, "msg": msg, "data": data})
 
 
+@handler_print.route("/api/getAnalyticjobs", methods=['GET'])
+def getAnalyticJobs():
+    return response(0, "", task.JobList)
+
+
+@handler_print.route("/api/upload", methods=["POST"])
+def uploadFile():
+    pass
+
+
+@handler_print.route("/api/createTask", methods=["POST"])
+def createTask():
+    pass
+
+
+@handler_print.route("/api/getTasks", methods=["GET"])
+def getTasks():
+    pass
+
+
 @handler_print.before_app_request
 def check():
-    print("before_print2")
+    print("before_print3")
--- app/server.py
+++ app/server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, send_from_directory, jsonify
 import handler
 import task
 
+
 # print(dir(handler), dir(__name__))
 app = Flask(__name__, static_folder="../web/build/static",
             template_folder="../web/build")
@@ -11,13 +12,8 @@ app.register_blueprint(handler.handler_print)
 
 @app.route("/", defaults={'path': ''})
 @app.route('/<path:path>')
-def main(path):
+def home(path):
     return render_template('index.html')
-
-
-@app.route("/api/getAnalyticjobs", methods=['GET'])
-def getAnalyticJobs():
-    return handler.response(0, "", task.JobList)
 
 
 if __name__ == "__main__":
